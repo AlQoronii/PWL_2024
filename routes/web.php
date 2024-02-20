819d1e5b857d95e4fbaaffbeb69956f2d26de01d
AlQoronii <fathurrhazaq@gmail.com>
2024-02-20 10:18:45 +0700

    Route with Parameter

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/user/{name}', function ($name){
+//     return 'Nama saya '.$name;
+// });
+
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-:".$commentId;
+});
+
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID - '.$id;
+});
+
+
+Route::get('/goldarah/{gol?}', function($gol='O'){
+    return 'Golongan Darah Anda - '.$gol;
+});
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function(){
+ return 'User/profile berubah menjadi profile ';
+})->name('profile');
+
